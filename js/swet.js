26c2afb323421a926fe8fbd79c299a44676c7490
d3ter <35612142+d3ter@users.noreply.github.com>
2020-11-22 14:37:27 -0500

    Add files via upload

---
+++ js/swet.js
@@ -0,0 +1,45 @@
+$(".text_secf").on("click",function(){
+    document.getElementById("img_secf").src="img/reloj.png";
+    Swal.fire({
+        icon: 'info', 
+        html: '<h3 class="text_color">¡No olvides escucharnos todos los domingos a las 10 AM por la radio Universidad de Pamplona 94.9 Fm! ;)</h3>', 
+        background : 'rgb(35 35 35)',
+        padding: '2rem',
+        timer: 6000,
+        timerProgressBar: true,
+        showConfirmButton: false,
+        customClass: {
+            timerProgressBar: 'timer-bar'
+        }   
+     })
+})
+
+$(".registro").on("click",function(){
+    Swal.fire({
+        icon: 'error',
+        html: '<h3 class="text_color">¡Upss! Resgistro no disponible por el momento :(</h3>', 
+        background : 'rgb(35 35 35)',
+        padding: '2rem',
+        timer: 5000,
+        showConfirmButton: false,
+        timerProgressBar: true,
+        customClass: {
+            timerProgressBar: 'timer-bar'
+        }
+     })
+})
+
+$(".sesion").on("click",function(){
+    Swal.fire({
+        icon: 'error',
+        html: '<h3 class="text_color">¡Upss! Inicio de session no disponible por el momento :(</h3>', 
+        background : 'rgb(35 35 35)',
+        padding: '2rem',
+        timer: 5000,
+        showConfirmButton: false,
+        timerProgressBar: true,
+        customClass: {
+            timerProgressBar: 'timer-bar'
+        }
+     })
+})
